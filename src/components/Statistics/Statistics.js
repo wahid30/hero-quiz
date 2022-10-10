03d0b8796ee7a4879f28ed9ef544a9d8c9f950fc
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-11 01:39:12 +0600

    rechart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,50 @@
 import React from "react";
-
+import { useLoaderData } from "react-router-dom";
+import { PureComponent } from "react";
+import {
+  ResponsiveContainer,
+  ComposedChart,
+  Line,
+  Area,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 const Statistics = () => {
+  const data = useLoaderData().data;
+  // console.log(datas);
   return (
-    <div>
-      <h2>this is statistics</h2>
+    <div style={{ width: "100%", height: 300 }}>
+      <ResponsiveContainer>
+        <ComposedChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="name" scale="band" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Area
+            type="monotone"
+            dataKey="total"
+            fill="#8884d8"
+            stroke="#8884d8"
+          />
+          <Bar dataKey="total" barSize={20} fill="#413ea0" />
+          <Line type="monotone" dataKey="total" stroke="#ff7300" />
+        </ComposedChart>
+      </ResponsiveContainer>
     </div>
   );
 };
