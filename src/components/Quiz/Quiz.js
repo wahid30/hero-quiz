6a2124013a88a7e9cdcbab5f80fb6fb8339d008e
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-11 16:09:07 +0600

    install tostify and icon functionality added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,17 +1,29 @@
-import React from "react";
+// import React, { useState } from "react";
 import "./Quiz.css";
 import { EyeIcon } from "@heroicons/react/24/solid";
 const Quiz = ({ quiz }) => {
   const { question, options, correctAnswer } = quiz;
-  // console.log(options);
+  // console.log(correctAnswer);
+  // const [checkIcon, setCheckIcon] = useState(false);
+  // const handleIconClicked = () => {
+  //   // console.log(correctAnswer);
+  // };
   return (
     <div>
       <div className="rounded-[10px] shadow-xl bg-green-100 my-5 p-5">
         <div className="flex justify-between items-center">
           <h2 className="font-bold">{question}</h2>
-          <button>
-            <EyeIcon className="w-[20px] hover:text-blue-700"></EyeIcon>
-          </button>
+          <div className="collapse">
+            <input type="checkbox" className="peer" />
+            <div className="collapse-title  text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content flex justify-end">
+              <p>
+                <EyeIcon className="w-[20px] text-black"></EyeIcon>
+              </p>
+            </div>
+            <div className="collapse-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+              <p>{correctAnswer}</p>
+            </div>
+          </div>
         </div>
         <div className="p-3 flex flex-col">
           {options.map((option, idx) => (
